ce836084881f2e7ee003499d4a363dbd35d075da
mengmi <lzlzlzhaha@163.com>
2020-09-21 02:20:09 -0400

    checkpoint updated

--- pytorch/recurrentVA_obj/eval_exp.py
+++ pytorch/recurrentVA_obj/eval_exp.py
@@ -44,7 +44,7 @@ click_steps = 17
 workers = 1
 decode_lengths = torch.ones(batch_size)*click_steps #constant number of mouse clicks
 
-checkpoint = 'models/checkpoint_2.pth.tar'  # model checkpoint
+checkpoint = 'models/checkpoint_4.pth.tar'  # model checkpoint
 #word_map_file = '/home/mengmi/Projects/Proj_context1/Datalist/ClassLabelList.txt'  # word map, ensure it's the same the data was encoded with and the model was trained with
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
 cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
